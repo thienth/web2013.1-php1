ac87a6d120d948786bc89a2548c3679352265963
thienth32 <thienth32@gmail.com>
2019-07-23 13:49:15 +0700

    UPDATE

---
+++ lesson6/lab3.php
@@ -0,0 +1,17 @@
+<?php 
+/**
+ * Tạo bảng products trong csdl lesson6 có cấu trúc như sau:
+ * id int pk ai
+ * product_name varchar 255
+ * image varchar 255
+ * price int
+ * detail text
+ * brand varchar 255
+ *
+ * thực hiện insert ít nhất 5 bản ghi vào bảng products
+ * code kết nối từ file lab3.php đến cơ sở dữ liệu
+ * sau đó thực hiện lấy toàn bộ dữ liệu từ bảng products và hiển thị ra dạng table
+ */
+
+
+ ?>
